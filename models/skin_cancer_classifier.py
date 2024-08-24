f03797062d773893fb8917fc386ec4b82cfbb3df
JoseFPortoles <69344137+JoseFPortoles@users.noreply.github.com>
2024-08-24 20:25:32 +0200

    Transforms

--- models/skin_cancer_classifier.py
+++ models/skin_cancer_classifier.py
@@ -9,14 +9,10 @@ class SkinCancerClassifier(nn.Module):
     def __init__(self, resnet_weights=ResNet50_Weights.IMAGENET1K_V2, unet_weights=VGG16_Weights, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.unet = UNet(weights=unet_weights, out_channels=1)
-        self.resnet = models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+        self.resnet = models.resnet50(weights=resnet_weights)
         self.resnet.fc = nn.Linear(2048, 2, True).apply(init_weights)
 
     def forward(self, x):
         mask = self.unet(x).expand(3, -1, -1)
         x = x * mask
         return self.resnet(x)
-
-if __name__ == '__main__':
-    model = SkinCancerClassifier()
-    print(model)
