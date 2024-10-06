f1a5267b78a17d7e73431fe57377f2642267641d
JoseFPortoles <69344137+JoseFPortoles@users.noreply.github.com>
2024-10-06 19:26:59 +0200

    Fix/improve split, dataset classes and train loop. Put scheduler in control of lr

--- datasets/isic_2024.py
+++ datasets/isic_2024.py
@@ -10,7 +10,7 @@ import io
 from datasets.helpers import get_metadata_row
 from datasets.splits import split_isic2024
 from transforms.transforms import transform_isic_2024
-import pandas
+import pandas as pd
 from torch.utils.tensorboard import SummaryWriter
 import torch.nn.functional as F
 from typing import Tuple
@@ -31,34 +31,50 @@ def extract_image(hdf5_data: h5py.Dataset) -> np.array:
     image_array = np.array(image)
     return image_array
 
-class ISIC2024Dataset(Dataset):
-    def __init__(self, seed: int, root_folder=ISIC_2024_ROOT, transform=None, split_ratio: Tuple[int,int,int]=(0.8, 0.1, 0.1), mode:str = 'train', writer: SummaryWriter=None):
+class ISIC2024Split:
+    def __init__(self, seed: int, root_folder=ISIC_2024_ROOT, split_ratio: Tuple[int,int,int]=(0.8, 0.1, 0.1), writer: SummaryWriter=None) -> None:
+        self.seed = seed
+        self.split_ratio = split_ratio
         self.writer = writer
-        annot_path = Path(root_folder)/"train-metadata.csv"
-        hdf5_path = Path(root_folder)/"train-image.hdf5"
-        self.train_df, self.val_df, self.test_df = split_isic2024(annot_path, seed, split_ratio)
-        try:
-            self.annot_df = {'train': self.train_df, 'val': self.val_df, 'test': self.test_df}[mode]
-        except ValueError as e:
-            print(f"{e}. Argument mode needs to be either 'train', 'val', or 'test'.")
-            
-        self.hdf5_file = h5py.File(hdf5_path, 'r')
+        self.annot_path = Path(root_folder)/"train-metadata.csv"
+        self.hdf5_path = Path(root_folder)/"train-image.hdf5"
+        self.annot_df = {}
+        self.hdf5_file = h5py.File(self.hdf5_path, 'r')
         self.hdf5_keys = [key for key in self.hdf5_file.keys()]
+    
+    def split(self):
+        self.annot_df['train'], self.annot_df['val'], self.annot_df['test'] = split_isic2024(self.annot_path, self.seed, self.split_ratio)
+    
+    def save(self, out_folder: str):
+        for key, df in self.annot_df.items():
+            filepath = Path(out_folder)/f'{key}_split.csv'
+            df.to_csv(filepath, index=True)
+
+    def load(self, in_folder: str):
+        for key in ['train', 'val', 'test']:
+            filepath = Path(in_folder)/f'{key}_split.csv'
+            self.annot_df[key] = pd.read_csv(filepath)
+
+class ISIC2024Dataset(Dataset):
+    def __init__(self, split: ISIC2024Split, transform=None, mode:str = 'train', writer: SummaryWriter=None):
+        self.hdf5_file = split.hdf5_file
+        self.hdf5_keys = split.hdf5_keys
+        self.subset_df = split.annot_df[mode]
         self.transform = transform
 
     def __getitem__(self, subset_index):
-        set_index = self.annot_df.index[subset_index]
+        set_index = self.subset_df.index[subset_index]
         img = extract_image(self.hdf5_file[self.hdf5_keys[set_index]][()])
         if self.transform:
             aug = self.transform(image=img)
             img = aug['image'].to(torch.float32)
-        metadata = get_metadata_row(self.annot_df, self.hdf5_keys[set_index])
+        metadata = get_metadata_row(self.subset_df, self.hdf5_keys[set_index])
         grading = torch.tensor(metadata['target'])
         grading = F.one_hot(grading, num_classes=2)
         return img, grading
         
     def __len__(self):
-        return len(self.annot_df)
+        return len(self.subset_df)
 
 if __name__ == '__main__':
     dataset = ISIC2024Dataset(42,transform=transform_isic_2024(224), mode='train')
--- scripts/train.py
+++ scripts/train.py
@@ -10,21 +10,30 @@ parser.add_argument('--batch_size', type=int, default=32, help='Batch size.')
 parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate.')
 parser.add_argument('--wd', type=float, default=1e-3, help='Weight decay.')
 parser.add_argument('--input_size', type=int, default=224, help='Lateral size for the (square) input image.')
-parser.add_argument('--unet_weights_path', type=str, default=None, help='U-Net weights file path. If =None then initialise net w/ Xavier function.')
+parser.add_argument('--unet_weights_path', type=str, default=None, help='U-Net initial weights path. If =None then initialise net w/ Xavier function.')
 parser.add_argument('--scc_weights_path', type=str, default=None, help='Saved complete network weights. If =None then initialise U-Net with xavier and ResNet with ImageNet weights.')
 parser.add_argument('--data_root', type=str, help='Dataset root folder path.')
 parser.add_argument('--output_path', type=str, default='./checkpoints', help='Folder where trained weights are saved.')
 parser.add_argument('--lr_scheduler_factor', type=float, default=0.1, help='lr reduction factor for the lr scheduler.')
-parser.add_argument('--lr_scheduler_patience', type=int, default=3, help='lr scheduler patience.')
+parser.add_argument('--lr_scheduler_patience', type=int, default=3, help='lr scheduler patience (in number of validation points, not epochs)')
 parser.add_argument('--num_workers', type=int, help='Num. workers for data loading.')
 parser.add_argument('--pin_memory', action='store_true', help='Use pin memory in data loading')
 parser.add_argument('--seed', type=int, help="Seed for all libraries")
+parser.add_argument('--split_dataset', action='store_true')
+parser.add_argument('--split_folder', type=str, help="Folder containing the respective .csv files with the split sets metadata")
+parser.add_argument('--split_ratio', type=str, help="Split ratio for (train, validation, test) sets")
+parser.add_argument('--num_val_points', type=int, help='Approximate number of validation points across training')
 
 args = parser.parse_args()
 
+split_ratio = eval(args.split_ratio)
+if type(split_ratio) is tuple and len(split_ratio) == 3:
+    print(f'Split dataset in train, val, test ratios {split_ratio}.\nMetadata files in {args.split_folder}')
+else:
+    raise ValueError("split_ratio must be a tuple with three floating point numbers")
 
 if __name__ == '__main__':
     seed_all(args.seed)
     model = SkinCancerClassifier()
-    train_loop(args.seed, args.num_epochs, args.batch_size, args.lr, args.wd, args.input_size, args.unet_weights_path, args.scc_weights_path, args.data_root, args.output_path, args.lr_scheduler_factor, args.lr_scheduler_patience, args.num_workers, args.pin_memory)
+    train_loop(args.seed, args.num_epochs, args.batch_size, args.lr, args.wd, args.input_size, args.unet_weights_path, args.scc_weights_path, args.data_root, args.output_path, args.lr_scheduler_factor, args.lr_scheduler_patience, args.num_workers, args.pin_memory, args.split_dataset, args.split_folder, split_ratio)
     
